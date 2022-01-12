d71cf97141c0afd98a42dda64083068169f5d4ef
Andrii Ivanichenko <ikenobiq@gmail.com>
2022-01-13 00:34:39 +0200

    fix

--- src/client/Filters/Filters.jsx
+++ src/client/Filters/Filters.jsx
@@ -6,30 +6,26 @@ import SalaryFilter from "./SalaryFilter/SalaryFilter";
 import JobExperienceFilter from "./JobExperienceFilter/JobExperienceFilter";
 import LanguageFilter from "./LanguageFilter/LanguageFilter";
 import TypeOfEmployment from "./TypeOfEmployment/TypeOfEmployment";
-import EducationFilter from "./EducationFilter/EducationFilter";
 
-const Filters = ({ withPhoto, onWithPhotoChange }) => {
-  return (
-    <div className={styles.filters}>
-      <div className={styles.filtersHeadingDiv}>
-        <h2 className={styles.filtersHeading}>Фильтры</h2>
-      </div>
-      <div className={styles.filtersMainDiv}>
-        <SummaryWithPhotoSelector
-          checked={withPhoto}
-          onChange={(checked) => {
-            onWithPhotoChange(checked);
-          }}
-        />
-        <AgeFilter />
-        <SexFilter />
-        <SalaryFilter />
-        <JobExperienceFilter />
-        <LanguageFilter />
-        <TypeOfEmployment />
-        <EducationFilter />
-      </div>
-    </div>
-  );
+const Filters = ({hasPhoto, onHasPhotoChange}) => {
+    return (
+        <div className={styles.filters}>
+            <div className={styles.filtersHeadingDiv}>
+                <h2 className={styles.filtersHeading}>Фильтры</h2>
+            </div>
+            <div className={styles.filtersMainDiv}>
+                <SummaryWithPhotoSelector
+                    checked={hasPhoto}
+                    onChange={(checked) => onHasPhotoChange(checked)}
+                />
+                <AgeFilter/>
+                <SexFilter/>
+                <SalaryFilter/>
+                <JobExperienceFilter/>
+                <LanguageFilter/>
+                <TypeOfEmployment/>
+            </div>
+        </div>
+    );
 };
 export default Filters;
--- src/pages/findEmployees/FindEmployees.jsx
+++ src/pages/findEmployees/FindEmployees.jsx
@@ -3,66 +3,59 @@ import Header from "../../client/Header";
 import CV from "../../client/summaries/Summaries";
 import Lobby from "./lobby/Lobby";
 import Candidates from "./сandidates/Сandidates";
-import { useEffect, useState } from "react";
-import { fetchAllSummaries, fetchFilteredSummaries } from "../../fetch/fetch";
+import {useEffect, useState} from "react";
+import {fetchAllSummaries, fetchFilteredSummaries} from "../../fetch/fetch";
 
-import { useMediaQuery } from "react-responsive";
+import {useMediaQuery} from "react-responsive";
 import useMedia from "../../client/useMedia";
 
 const PageSize = {
-  desktop: 9,
-  mobile: 5,
-};
+    desktop: 9,
+    mobile: 5
+}
 
 const FindEmployees = () => {
-  const [summaries, setSummaries] = useState([]);
-  const [totalSummaryCount, setTotalSummaryCount] = useState(0);
+    const [summaries, setSummaries] = useState([]);
+    const [totalSummaryCount, setTotalSummaryCount] = useState(0);
 
-  const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
 
-  const [hasPhoto, setHasPhoto] = useState(false);
+    const [hasPhoto, setHasPhoto] = useState(false);
 
-  const { isMobile } = useMedia();
-  const pageSize = isMobile ? PageSize.mobile : PageSize.desktop;
+    const {isMobile} = useMedia()
+    const pageSize = isMobile ? PageSize.mobile : PageSize.desktop
 
-  function handleWithPhotoChanged(checked) {
-    if (hasPhoto !== checked) setCurrentPage(1);
+    function handleWithPhotoChanged(checked) {
+        if (hasPhoto !== checked)
+            setCurrentPage(1)
 
-    setHasPhoto(checked);
-  }
+        setHasPhoto(checked)
+    }
 
-  useEffect(() => {
-    const getSummaries = async () => {
-      try {
-        const resp = await (hasPhoto
-          ? fetchFilteredSummaries(currentPage, hasPhoto, pageSize)
-          : fetchAllSummaries(currentPage, pageSize));
-        const { results, total_count } = resp;
-        setSummaries(results);
-        setTotalSummaryCount(total_count);
-      } catch (error) {}
-    };
-    getSummaries();
-  }, [currentPage, hasPhoto, pageSize]);
+    useEffect(() => {
+        const getSummaries = async () => {
+            try {
+                const resp = await (hasPhoto ? fetchFilteredSummaries(currentPage, hasPhoto, pageSize) : fetchAllSummaries(currentPage, pageSize));
+                const {results, total_count} = resp
+                setSummaries(results)
+                setTotalSummaryCount(total_count)
+            } catch (error) {
+            }
+        };
+        getSummaries();
+    }, [currentPage, hasPhoto, pageSize]);
 
-  return (
-    <>
-      <Header />
-      <Lobby />
-      <Candidates
-        hasPhoto={hasPhoto}
-        onHasPhotoChange={handleWithPhotoChanged}
-      />
-      <CV
-        summaries={summaries}
-        totalSummaryCount={totalSummaryCount}
-        currentPage={currentPage}
-        pageSize={pageSize}
-        onCurrentPageChanged={setCurrentPage}
-      />
-      <Footer />
-    </>
-  );
+
+    return (
+        <>
+            <Header/>
+            <Lobby/>
+            <Candidates hasPhoto={hasPhoto} onHasPhotoChange={handleWithPhotoChanged}/>
+            <CV summaries={summaries} totalSummaryCount={totalSummaryCount} currentPage={currentPage} pageSize={pageSize}
+                onCurrentPageChanged={setCurrentPage}/>
+            <Footer/>
+        </>
+    );
 };
 
 export default FindEmployees;
